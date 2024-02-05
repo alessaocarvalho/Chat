d7d836e9941fc212490694a671331c34253a30f6
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-02-04 22:26:04 -0300

    Mensagem enviada

--- servidor.js
+++ servidor.js
@@ -2,4 +2,16 @@ const http = require('http')
 const express = require('express')
 const application = express()
 
-const servidorHttp = http.createServer(aplicacao)
+const servidorHttp = http.createServer(application)
+const io = require('socket.io')(servidorHttp)
+
+io.addListener('connection', (socket) => {
+    console.log('Usuário conectado')
+    socket.addListener('Nova mensagem', (msg) => {
+        io.emit('Nova mensagem', msg)
+    })
+})
+
+application.use(express.static('public'))
+
+servidorHttp.listen(1010)
